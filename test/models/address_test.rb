b85e387b996d9e1b4bf97ae2b0ef4e52c4bf63ef
Frank DelPidio <frankdaedalus@gmail.com>
2017-07-21 19:03:20 -0400

    Add validation tests for Address

--- test/models/address_test.rb
+++ test/models/address_test.rb
@@ -4,4 +4,18 @@ class AddressTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @address = Address.new(query: '123 Sesame street')
+  end
+
+  test "valid address" do
+    @address.valid?
+  end
+
+  test "invalid without query" do
+    @address.query = nil
+    refute @address.valid?, 'saved address without query'
+    assert_not_nil @address.errors[:query], 'no validation error for query not present'
+  end
 end
