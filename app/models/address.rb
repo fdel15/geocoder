a9b87a0e574f974e4176c709edfadb09f80ae358
Frank DelPidio <frankdaedalus@gmail.com>
2017-07-22 16:10:17 -0400

    Create custom save method for Address model

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,10 @@
 class Address < ApplicationRecord
   validates :query, presence: true
+
+  def save
+    existing_address = Address.where address: self.address
+    self.new_address = false if existing_address.present?
+    super
+  end
+
 end
