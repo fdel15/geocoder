a33453ca8e5ef162121a5f01ba3b7459753b91e8
Frank DelPidio <frankdaedalus@gmail.com>
2017-07-22 16:07:18 -0400

    Create create action for address

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -4,6 +4,31 @@ class AddressesController < ApplicationController
   end
 
   def create
-    render json: "Hello World"
+    @query = sanitize_form_input(params[:query])
+    @address = Address.find_by query: @query
+
+    if @address.nil?
+      coordinates = GoogleClient.new(@query)
+      @new_address = true
+      @address = Address.new(query: @query, longitude: coordinates.longitude, latitude: coordinates.latitude, address: coordinates.formatted_address)
+      @address.save
+    end
+
+    respond_to do |format|
+      format.html { redirect_to :root }
+      format.js { render template: "addresses/error_no_results.js.erb" } if @address.zero_results
+      format.js
+    end
+  end
+
+
+private
+
+  def address_params
+    params.permit(:query, :latitude, :longitude, :address, :zero_results)
+  end
+
+  def sanitize_form_input(input)
+    input.split(' ').join('+')
   end
 end
